8e14ef47ed39ad00d330b16ff0b6f7dbfd146ee1
Luiz_Seixas <ticoluizgustavo@outlook.com>
2023-01-03 16:45:40 -0300

    ensure PgUserAccountRepository creates an account if id is undefined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -42,4 +42,17 @@ describe('PgUserAccountRepository', () => {
       expect(account).toBe(undefined)
     })
   })
+
+  describe('saveWithFacebook', () => {
+    it('should create an account if id is undefined', async () => {
+      await sut.saveWithFacebook({
+        name: 'any_name',
+        email: 'any_email',
+        facebookId: 'any_fb_id'
+      })
+      const pgUser = await pgUserRepo.findOne({ where: { email: 'any_email' } })
+
+      expect(pgUser?.id).toBe(1)
+    })
+  })
 })
